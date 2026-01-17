b5b6a35167d2a4ca2130d625af605eb174a542dc
Shruti Chougale <shrutichougale139@gmail.com>
2026-01-17 23:26:27 +0530

    Add files via upload

---
+++ Avg.java
@@ -0,0 +1,9 @@
+public class Avg{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int avg = (a + b) / 2;
+        System.out.println("Average = " + avg);
+    }
+ }
